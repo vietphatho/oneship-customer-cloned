8de68dd9f318c50cae8150704aee459d19158a33
Hồ Việt Phát <dev01@oneship.app>
2026-04-29 15:11:55 +0700

    improve shop selection button

--- lib/features/shop_home/presentation/widgets/shop_selection_button.dart
+++ lib/features/shop_home/presentation/widgets/shop_selection_button.dart
@@ -1,8 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oneship_customer/core/base/base_import_components.dart';
-import 'package:oneship_customer/core/utils/string_utils.dart';
 import 'package:oneship_customer/di/injection_container.dart';
+import 'package:oneship_customer/features/shop_home/domain/entities/get_shops_entity.dart';
 import 'package:oneship_customer/features/shop_home/presentation/bloc/shop_bloc.dart';
 import 'package:oneship_customer/features/shop_home/presentation/bloc/shop_state.dart';
 
@@ -18,107 +18,15 @@ class ShopSelectionButton extends StatelessWidget {
       builder: (context, state) {
         final _shopLogoUrl = state.currentShop?.shopLogo;
 
-        return Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: AppColors.primary,
-              width: AppDimensions.smallBorderStroke,
-            ),
-            borderRadius: AppDimensions.largeBorderRadius,
-          ),
-          child: DropdownButton(
-            items:
-                state.shopsResource.data?.data
-                    .map(
-                      (e) => DropdownMenuItem(
-                        value: e,
-                        child: PrimaryText(
-                          e.shopName,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    )
-                    .toList(),
-            value: state.currentShop,
-            underline: SizedBox(),
-            onChanged: (value) {
-              if (value != null) {
-                _shopBloc.changeShop(value);
-              }
-            },
-          ),
-        );
-
-        return DropdownMenu(
-          trailingIcon: const Icon(
-            Icons.keyboard_arrow_down,
-            color: AppColors.neutral6,
-          ),
-          selectedTrailingIcon: const Icon(
-            Icons.keyboard_arrow_up,
-            color: AppColors.neutral6,
-          ),
-          enableSearch: false,
-          initialSelection: state.currentShop,
-          dropdownMenuEntries:
-              state.shopsResource.data?.data
-                  .map((e) => DropdownMenuEntry(label: e.shopName, value: e))
-                  .toList() ??
-              [],
-          menuStyle: MenuStyle(
-            padding: WidgetStateProperty.all(const EdgeInsets.only(top: 8)),
-            elevation: WidgetStateProperty.all(4),
-            // backgroundColor: WidgetStateProperty.all(
-            //   colorScheme.surfaceContainerHigh,
-            // ),
-            visualDensity: VisualDensity.compact,
-            shape: WidgetStateProperty.all(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            ),
-          ),
-          textStyle: AppTextStyles.labelSmall.copyWith(
-            overflow: TextOverflow.ellipsis,
-          ),
-          expandedInsets: EdgeInsets.zero,
-          // onChanged: (value) {},
-        );
-
-        return Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: AppDimensions.xSmallSpacing,
-            vertical: AppDimensions.xxSmallSpacing,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(color: AppColors.neutral6),
-            borderRadius: AppDimensions.mediumBorderRadius,
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Icon(
-              //   CupertinoIcons.house,
-              //   color: AppColors.primary,
-              //   size: AppDimensions.xSmallIconSize,
-              // ),
-              if (_shopLogoUrl != null)
-                _ShopAvatar(url: StringUtils.getImgUrl(_shopLogoUrl)!),
-              AppSpacing.horizontal(AppDimensions.xxSmallSpacing),
-              Expanded(
-                child: PrimaryText(
-                  state.currentShop?.shopName,
-                  overflow: TextOverflow.ellipsis,
-                  style: AppTextStyles.labelMedium,
-                ),
-              ),
-              AppSpacing.horizontal(AppDimensions.xxSmallSpacing),
-              Icon(
-                Icons.keyboard_arrow_down_rounded,
-                color: AppColors.neutral6,
-                size: AppDimensions.xSmallIconSize,
-              ),
-            ],
-          ),
+        return _ShopDropdownButton<ShopEntity>(
+          items: state.shopsResource.data?.data ?? [],
+          value: state.currentShop,
+          labelBuilder: (shop) => shop.shopName,
+          onChanged: (value) {
+            if (value != null) {
+              _shopBloc.changeShop(value);
+            }
+          },
         );
       },
     );
@@ -141,6 +49,72 @@ class _ShopAvatar extends StatelessWidget {
           fit: BoxFit.cover,
         ),
         borderRadius: AppDimensions.xSmallBorderRadius,
+      ),
+    );
+  }
+}
+
+class _ShopDropdownButton<T> extends StatelessWidget {
+  final List<T> items;
+  final T? value;
+  final String Function(T) labelBuilder;
+  final void Function(T?) onChanged;
+
+  final EdgeInsets padding;
+  final Color backgroundColor;
+  final double iconSize;
+  final Color iconColor;
+  final double borderRadius;
+
+  const _ShopDropdownButton({
+    super.key,
+    required this.items,
+    required this.value,
+    required this.labelBuilder,
+    required this.onChanged,
+    this.padding = const EdgeInsets.symmetric(
+      horizontal: AppDimensions.smallSpacing,
+      vertical: AppDimensions.xSmallSpacing,
+    ),
+    this.backgroundColor = Colors.white,
+    this.iconSize = AppDimensions.xSmallIconSize,
+    this.iconColor = AppColors.neutral5,
+    this.borderRadius = AppDimensions.largeRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(borderRadius),
+        // border: Border.all(color: Colors.grey),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<T>(
+          value: value,
+          isExpanded: true,
+          isDense: true,
+          icon: Icon(
+            Icons.keyboard_arrow_down,
+            size: iconSize,
+            color: iconColor,
+          ),
+          items:
+              items.map((item) {
+                return DropdownMenuItem<T>(
+                  value: item,
+                  child: PrimaryText(
+                    labelBuilder(item),
+                    style: AppTextStyles.labelSmall,
+                    maxLine: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                );
+              }).toList(),
+          onChanged: onChanged,
+        ),
       ),
     );
   }
