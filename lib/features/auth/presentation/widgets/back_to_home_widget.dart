b1dab1bd2cab2c3ab3bcbdca10bfd03142d50fc1
Thanh Thien <thien.ta@onexway.io>
2026-05-05 17:35:42 +0700

    change button search order to back home

--- lib/features/auth/presentation/widgets/back_to_home_widget.dart
+++ lib/features/auth/presentation/widgets/back_to_home_widget.dart
@@ -1,10 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:oneship_customer/core/base/base_import_components.dart';
 import 'package:oneship_customer/core/base/components/primary_animated_pressable_widget.dart';
-import 'package:oneship_customer/core/navigation/route_name.dart';
 
-class SearchOrdersInfoWidget extends StatelessWidget {
-  const SearchOrdersInfoWidget({super.key});
+class BackToHomeWidget extends StatelessWidget {
+  const BackToHomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +11,12 @@ class SearchOrdersInfoWidget extends StatelessWidget {
       child: Center(
         child: PrimaryAnimatedPressableWidget(
           onTap: () {
-            context.push(RouteName.orderTrackingPage);
+            context.pop();
           },
           child: Padding(
             padding: AppDimensions.mediumPaddingAll,
             child: PrimaryText(
-              "order_tracking".tr(),
+              "back_to_home".tr(),
               style: AppTextStyles.labelLarge.copyWith(
                 decoration: TextDecoration.underline,
               ),
