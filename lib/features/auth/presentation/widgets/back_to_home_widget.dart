b3f196868fcfd0d6882d1622ec1d50862e22750a
Hồ Việt Phát <dev01@oneship.app>
2026-05-06 16:05:07 +0700

    update finance feature

--- lib/features/auth/presentation/widgets/back_to_home_widget.dart
+++ lib/features/auth/presentation/widgets/back_to_home_widget.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:oneship_customer/core/base/base_import_components.dart';
 import 'package:oneship_customer/core/base/components/primary_animated_pressable_widget.dart';
+import 'package:oneship_customer/core/navigation/route_name.dart';
 
 class BackToHomeWidget extends StatelessWidget {
   const BackToHomeWidget({super.key});
@@ -11,7 +12,11 @@ class BackToHomeWidget extends StatelessWidget {
       child: Center(
         child: PrimaryAnimatedPressableWidget(
           onTap: () {
-            context.pop();
+            if (context.canPop()) {
+              context.pop();
+            } else {
+              context.pushReplacement(RouteName.homePage);
+            }
           },
           child: Padding(
             padding: AppDimensions.mediumPaddingAll,
